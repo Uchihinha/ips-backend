09a0d6fee48f66f81ea842bb41a5f48271fb3f5f
Gabriel Jorge <gabriel.jorge.pereira@outlook.com>
2022-02-28 17:07:48 -0300

    Setting up Badges structure

--- database/seeders/LessonSeeder.php
+++ database/seeders/LessonSeeder.php
@@ -5,18 +5,17 @@ namespace Database\Seeders;
 use App\Models\Lesson;
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
+class LessonSeeder extends Seeder
 {
     /**
-     * Seed the application's database.
+     * Run the database seeds.
      *
      * @return void
      */
     public function run()
     {
-        $this->call([
-            LessonSeeder::class,
-            BadgeSeeder::class,
-        ]);
+        Lesson::factory()
+            ->count(20)
+            ->create();
     }
 }
