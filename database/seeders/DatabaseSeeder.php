2d3bfd2648e7cfc1301544e7485ec1d8c7dac7fd
Gabriel Jorge <gabriel.jorge.pereira@outlook.com>
2022-03-01 03:25:55 -0300

    Setting up Heroku server and Global Seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,6 @@
 
 namespace Database\Seeders;
 
-use App\Models\Lesson;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -17,6 +16,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             LessonSeeder::class,
             BadgeSeeder::class,
+            GeneralSeeder::class
         ]);
     }
 }
